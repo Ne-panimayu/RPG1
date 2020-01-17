465be11659a508874d3089c67159c844bd813644
Ne-panimayu <bog.shkel@gmail.com>
2020-01-17 20:24:40 +0200

    initial commit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    GameObject canvasMain;
+
+    [SerializeField]
+    GameObject canvasLevel;
+
+    public int level;
+
+    public void SelectLevel1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Selectlevel2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void SelectLevel3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+
+    public void LevelSelection()
+    {
+
+        canvasMain.SetActive(false);
+        canvasLevel.SetActive(true);
+
+    }
+
+    public void Back()
+    {
+
+        canvasMain.SetActive(true);
+        canvasLevel.SetActive(false);
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/OnTriggerPlaySFX.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OnTriggerPlaySFX : MonoBehaviour
+{
+    public AudioClip clip;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            GetComponent<AudioSource>().PlayOneShot(clip);
+        }
+    }
+}
